d420644beb8f9276c1946e50b222a0e346422661
EmmanuelSantizFelipe <emmanuel.07.01@hotmail.com>
2020-09-23 22:47:24 -0500

    ready

--- conf.js
+++ conf.js
@@ -1,10 +1,10 @@
 let config = {
     databasedev() {
         return {
-            host: 'dev-database-kuik.c5ermeo2cnt4.us-east-2.rds.amazonaws.com',
-            user: 'admin',
-            password: 'kUyK4dm1n',
-            database: 'kuik',
+            host: 'YourHost',
+            user: 'user',
+            password: 'password',
+            database: 'database',
             waitForConnections: true,
             connectionLimit: 10,
             queueLimit: 0
